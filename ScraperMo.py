7dc1ffbe68c9c8637e061819c9ae477ddca86931
MohamedEl1 <60153848+MohamedEl1@users.noreply.github.com>
2020-02-14 21:42:36 +0000

    1.1 Web scraper that scrapes bike data from the API with comments

--- ScraperMo.py
+++ ScraperMo.py
@@ -1,9 +1,16 @@
 import requests
 import json
 
+# Connect to the URL
 url = "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=b0979801c4ff351dc53e4bf7120f76de43e10959"
 
 response = requests.get(url)
+
+# Read the output
 data = response.text
+
+# converting the string to JSON
 parsed = json.loads(data)
+
+# “pretty print” to make it more readable
 print(json.dumps(parsed, indent=4))
