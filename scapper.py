fb8638b727ecd284a623067b8c32539632c709f5
olawalebmg <60133260+olawalebmg@users.noreply.github.com>
2020-02-15 12:13:20 +0000

    olawale

--- scapper.py
+++ scapper.py
@@ -1,13 +1,14 @@
 import requests
-#Api to get the data
-bikes_url= "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=b0979801c4ff351dc53e4bf7120f76de43e10959"
+#Api to get the data from
+bikes_url="https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=b0979801c4ff351dc53e4bf7120f76de43e10959"
 weather_url=""
 
 # function to get data save in specified format
 def get_data():
-    bike_data= requests.get(bikes_url)
+    bike_data= request.get(bikes_url)
     file_type= bike_data.json()
-    return file_type
+    print(file_type)
 
 
 
+get_data()
